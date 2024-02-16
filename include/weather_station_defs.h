d2e37f24baef8fb89801c94ea3f9d8f936f997b1
Jacob S <jsimeone0105@gmail.com>
2024-02-16 15:24:53 -0600

    Set up platformIO project

---
+++ include/weather_station_defs.h
@@ -0,0 +1,65 @@
+#ifndef __WEATHER_STATION_DEFS_H__
+#define __WEATHER_STATION_DEFS_H__
+
+/**
+ * @brief Data structure to hold all of our weather station data
+ * 
+ * Why use a data structure?
+ * - More organized
+ * - Contigous (side-by-side) in memory
+ * 
+ */
+typedef struct {
+    float temp_c;
+    float pres_kpa;
+    float humid;
+    bool isRaining;
+    bool isSunny;
+} weather_data_t;
+
+/**
+ * @brief Initialize all sensors on the weather station
+ * 
+ * @return true -> Successful initialization
+ * @return false -> Initialization failed
+ */
+bool ws_init_sensors();
+
+/**
+ * @brief Read temperature from a sensor
+ * 
+ * @return float Temperature, in degrees celsius
+ */
+float ws_get_temperature();
+
+/**
+ * @brief Read humidity from a sensor
+ * 
+ * @return float Humidity, as a percent
+ */
+float ws_get_humidity();
+
+/**
+ * @brief Read pressure from a sensor
+ *
+ * @return float Pressure, in kPa
+ */
+float ws_get_pressure();
+
+/**
+ * @brief Check if it is currently raining
+ * 
+ * @return true -> Currently raining
+ * @return false -> Not currently raining
+ */
+bool ws_is_raining();
+
+/**
+ * @brief Check if it is currently sunny out
+ * 
+ * @return true -> Currently sunny
+ * @return false -> Not currently sunny
+ */
+bool ws_is_sunny();
+
+#endif
